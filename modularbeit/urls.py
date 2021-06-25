66bc4b44b923f90487cecccb991ac832c439231d
Suban Hackit <suban@hackit.ch>
2021-06-25 06:58:10 +0200

    make-public-aware

---
+++ modularbeit/urls.py
@@ -0,0 +1,44 @@
+"""modularbeit URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
+#OpenAPI Stuff mit drf yasg
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Vertiefungsarbeit API",
+      default_version='v1',
+      description="Modularbeit Subakeesan Kanapathipillai",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('api/v1/', include('pvs_suban.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('doc/schema.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-ui'),
+    path('admin/', admin.site.urls),
+]
---
+++ pvs_suban/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.urls import reverse
+from django.db.models import Count
+
+from .models import Contact, Address, Country, Invoice, InvoicePosition
+# Register your models here.
+
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    extra = 0
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [AddressInline]
+    list_display = ['name', 'type', 'address_count']
+    search_fields = ['name']
+    list_filter = ['type']
+
+    def address_count(self, obj):
+        count = obj.addresses.count()
+        return count
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value']
+    search_fields = ['value', 'key']
+
+
+class InvoposInline(admin.StackedInline):
+    model = InvoicePosition
+    extra = 0
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date','due', 'total', 'contact_name']
+    inlines = [InvoposInline]
+    search_fields = ['title', 'address__contact__name']
+
+    def contact_name(self, obj):
+        return obj.address.contact.name
+    contact_name.short_description = 'contact name'
