7c46d716e25e72365025a00470e81cf1806bae84
Suban Hackit <37937252+hackit90@users.noreply.github.com>
2021-06-25 07:21:15 +0200

    Add files via upload

---
+++ pvs_suban/migrations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PvsSubanConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pvs_suban'
---
+++ pvs_suban/migrations/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from .models import Contact, InvoicePosition, Address, Invoice, Country
+
+#Adresse integrieren
+class AddressNestedSerializer(ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['street', 'zip', 'city', 'country', 'id']
+class ContactSerializer(ModelSerializer):
+    addresses = AddressNestedSerializer(many=True)
+    class Meta:
+        model = Contact
+        fields = ['type', 'salutation', 'name', 'email','addresses', 'id']
+        read_only_fields = ['addresses']
+
+#InvoicePosition integrieren
+class InvoicePositionNestedSerializer(ModelSerializer):
+    class Meta:
+        model = InvoicePosition
+        fields = '__all__'
+class InvoiceSerializer(ModelSerializer):
+     InvoicePositions = InvoicePositionNestedSerializer(many=True)
+     total_amount = serializers.FloatField(source='total')
+
+     class Meta:
+         model = Invoice
+         fields = ['title', 'body', 'date', 'due', 'condition', 'InvoicePositions', 'total_amount', 'id']
+         read_only_fields = ['InvoicePositions']
+
+class InvoicePositionSerializer(ModelSerializer):
+    class Meta:
+        model = InvoicePosition
+        fields = '__all__'
+
+class AddressSerializer(ModelSerializer):
+    country_name = serializers.SerializerMethodField(source='get_country_name')
+    contact = serializers.SerializerMethodField(source='get_contact')
+    class Meta:
+        model = Address
+        fields = ['street', 'zip', 'city', 'invoices', 'contact', 'country_name', 'id']
+    def get_country_name(self, obj):
+        return obj.country.value
+    def get_contact(self, obj):
+        return obj.contact.name
+
+class CountrySerializer(ModelSerializer):
+    class Meta:
+        model = Country
+        fields = '__all__'
